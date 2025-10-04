e95bf2bfc58f3bf0384e3fdaa0232cde7fb5c3f3
Taylor Brigman <taylorbrigman7@gmail.com>
2025-10-04 12:38:16 -0500

    Commit

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const toggleBtn = document.getElementById('toggle-theme');
 toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark');
 });
+// Set current year in the footer
+const y = document.getElementById('year');
+if (y) y.textContent = new Date().getFullYear();
